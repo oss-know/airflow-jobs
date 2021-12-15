932df05127d4e081205038e485e1d1bcfa93af59
fivestarsky <fivestarsky@gmail.com>
2021-12-16 00:08:03 +0800

    docker image sync dev version

--- build_airflow_python_libs_docker_images.sh
+++ build_airflow_python_libs_docker_images.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker build . -f Dockerfile --tag airflowjobs/airflow:0.0.5-python3.8
+docker build . -f Dockerfile --tag airflowjobs/airflow:dev-python3.8
+#docker build . -f Dockerfile --tag airflowjobs/airflow:0.0.5-python3.8
