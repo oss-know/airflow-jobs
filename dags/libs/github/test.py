9740f0de40b7682b4b35995099d56e2bf927b839
fivestarsky <fivestarsky@gmail.com>
2021-12-20 21:24:25 +0800

    add init sync github issues comments func

--- dags/libs/github/test.py
+++ dags/libs/github/test.py
@@ -3,10 +3,8 @@ import requests
 import time
 from opensearchpy import OpenSearch
 from ..util.base import github_headers
-from ..a.b.demo import hello
 
 def test_github_headers():
     print(github_headers)
-    hello()
 
     return "End load_github_profile-return"
