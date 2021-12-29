6d8391093877d71cf73cfceaab01c2e4170a47fc
fivestarsky <fivestarsky@gmail.com>
2021-12-29 23:00:27 +0800

    modify sleep by github api

--- dags/libs/github/sync_issues.py
+++ dags/libs/github/sync_issues.py
@@ -88,7 +88,7 @@ def sync_github_issues(github_tokens, opensearch_conn_info, owner, repo):
 
     for page in range(1, 10000):
         # Token sleep
-        time.sleep(random.uniform(0.1, 0.5))
+        time.sleep(random.uniform(0.1, 0.2))
 
         req = github_api.get_github_issues(http_session=session,
                                            github_tokens_iter=github_tokens_iter,
