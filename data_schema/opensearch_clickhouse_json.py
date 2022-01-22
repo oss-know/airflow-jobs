a0726d452edfeac088bf2d6a13b34395cab49fc2
ynang <1844677457@qq.com>
2022-01-22 21:19:40 +0800

    供马麟参考的clickhouse关于opensearch的建表json
    来源：gits\github_commits\github_issues_comments(字段performed_via_github_app待确认)\github_issues(字段performed_via_github_app待确认)

---
+++ data_schema/opensearch_clickhouse_json.py
@@ -0,0 +1,317 @@
+gits: {
+    "message": "",
+    "hexsha": "",
+    "parents": ["", ""],
+    "author_tz": 0,
+    "committer_tz": 0,
+    "author_name": "",
+    "author_email": "",
+    "committer_name": "",
+    "committer_email": "",
+    "authored_date": "",
+    "authored_timestamp": 0,
+    "committed_date": "",
+    "committed_timestamp": 0,
+    "files": [{
+        "insertions": 0,
+        "deletions": 0,
+        "lines": 0,
+        "file_name": ""
+    }, {"insertions": 0,
+        "deletions": 0,
+        "lines": 0,
+        "file_name": ""}],
+    "total": {
+        "insertions": 0,
+        "deletions": 0,
+        "lines": 0,
+        "files": 0
+    },
+    "if_merged": False,
+    "type": ""
+}
+github_commits: {"sha": "",
+"node_id": "",
+"commit": {
+    "author": {
+        "name": "",
+        "email": "",
+        "date": ""
+    },
+    "committer": {
+        "name": "",
+        "email": "",
+        "date": ""
+    },
+    "message": "",
+    "tree": {
+        "sha": "",
+        "url": ""
+    },
+    "url": "",
+    "comment_count": 0,
+    "verification": {
+        "verified": False,
+        "reason": "",
+        "signature": "",
+        "payload": ""
+    }
+},
+"url": "",
+"html_url": "",
+"comments_url": "",
+"author": {
+    "login": "",
+    "id": 0,
+    "node_id": "",
+    "avatar_url": "",
+    "gravatar_id": "",
+    "url": "",
+    "html_url": "",
+    "followers_url": "",
+    "following_url": "",
+    "gists_url": "",
+    "starred_url": "",
+    "subscriptions_url": "",
+    "organizations_url": "",
+    "repos_url": "",
+    "events_url": "",
+    "received_events_url": "",
+    "type": "",
+    "site_admin": False
+},
+"committer": {
+    "login": "",
+    "id": 0,
+    "node_id": "",
+    "avatar_url": "",
+    "gravatar_id": "",
+    "url": "",
+    "html_url": "",
+    "followers_url": "",
+    "following_url": "",
+    "gists_url": "",
+    "starred_url": "",
+    "subscriptions_url": "",
+    "organizations_url": "",
+    "repos_url": "",
+    "events_url": "",
+    "received_events_url": "",
+    "type": "",
+    "site_admin": False
+},
+"parents": [
+    {
+        "sha": "",
+        "url": "",
+        "html_url": ""
+    }
+]}
+
+github_issues_comments:{
+    "url" : "",
+    "html_url" : "",
+    "issue_url" : "",
+    "id" : 0,
+    "node_id" : "",
+    "user" : {
+      "login" : "",
+      "id" : 0,
+      "node_id" : "",
+      "avatar_url" : "",
+      "gravatar_id" : "",
+      "url" : "",
+      "html_url" : "",
+      "followers_url" : "",
+      "following_url" : "",
+      "gists_url" : "",
+      "starred_url" : "",
+      "subscriptions_url" : "",
+      "organizations_url" : "",
+      "repos_url" : "",
+      "events_url" : "",
+      "received_events_url" : "",
+      "type" : "",
+      "site_admin" : false
+    },
+    "created_at" : "",
+    "updated_at" : "",
+    "author_association" : "",
+    "body" : "",
+    "reactions" : {
+      "url" : "",
+      "total_count" : 0,
+      "+1" : 0,
+      "-1" : 0,
+      "laugh" : 0,
+      "hooray" : 0,
+      "confused" : 0,
+      "heart" : 0,
+      "rocket" : 0,
+      "eyes" : 0
+    },
+    "performed_via_github_app" : null
+  }
+
+
+
+
+github_issues:{
+    "url" : "",
+    "repository_url" : "",
+    "labels_url" : "",
+    "comments_url" : "",
+    "events_url" : "",
+    "html_url" : "",
+    "id" : 0,
+    "node_id" : "",
+    "number" : 0,
+    "title" : "",
+    "user" : {
+      "login" : "",
+      "id" : 0,
+      "node_id" : "",
+      "avatar_url" : "",
+      "gravatar_id" : "",
+      "url" : "",
+      "html_url" : "",
+      "followers_url" : "",
+      "following_url" : "",
+      "gists_url" : "",
+      "starred_url" : "",
+      "subscriptions_url" : "",
+      "organizations_url" : "",
+      "repos_url" : "",
+      "events_url" : "",
+      "received_events_url" : "",
+      "type" : "",
+      "site_admin" : false
+    },
+    "labels" : [
+      {
+        "id" : 0,
+        "node_id" : "",
+        "url" : "",
+        "name" : "",
+        "color" : "",
+        "default" : false,
+        "description" : ""
+      }
+    ],
+    "state" : "",
+    "locked" : false,
+    "assignee" : {
+        "login": "",
+        "id": 0,
+        "node_id": "",
+        "avatar_url": "",
+        "gravatar_id": "",
+        "url": "",
+        "html_url": "",
+        "followers_url": "",
+        "following_url": "",
+        "gists_url": "",
+        "starred_url": "",
+        "subscriptions_url": "",
+        "organizations_url": "",
+        "repos_url": "",
+        "events_url": "",
+        "received_events_url": "",
+        "type": "",
+        "site_admin": false
+      },
+    "assignees" : [
+        {
+          "login": "",
+          "id": 0,
+          "node_id": "",
+          "avatar_url": "",
+          "gravatar_id": "",
+          "url": "",
+          "html_url": "",
+          "followers_url": "",
+          "following_url": "",
+          "gists_url": "",
+          "starred_url": "",
+          "subscriptions_url": "",
+          "organizations_url": "",
+          "repos_url": "",
+          "events_url": "",
+          "received_events_url": "",
+          "type": "",
+          "site_admin": false
+        }
+      ],
+    "milestone" : {
+        "url": "",
+        "html_url": "",
+        "labels_url": "",
+        "id": 0,
+        "node_id": "",
+        "number": 1,
+        "state": "",
+        "title": "",
+        "description": "",
+        "creator": {
+          "login": "",
+          "id": 0,
+          "node_id": "",
+          "avatar_url": "",
+          "gravatar_id": "",
+          "url": "",
+          "html_url": "",
+          "followers_url": "",
+          "following_url": "",
+          "gists_url": "",
+          "starred_url": "",
+          "subscriptions_url": "",
+          "organizations_url": "",
+          "repos_url": "",
+          "events_url": "",
+          "received_events_url": "",
+          "type": "",
+          "site_admin": false
+        },
+        "open_issues": 0,
+        "closed_issues": 0,
+        "created_at": "",
+        "updated_at": "",
+        "closed_at": "",
+        "due_on": ""
+      },
+    "comments" : 0,
+    "created_at" : "",
+    "updated_at" : "",
+    "closed_at" : "",
+    "author_association" : "",
+    "active_lock_reason" : "",
+    "draft" : false,
+    "pull_request" : {
+      "url" : "",
+      "html_url" : "",
+      "diff_url" : "",
+      "patch_url" : "",
+      "merged_at" : ""
+    },
+    "body" : "",
+    "reactions" : {
+      "url" : "",
+      "total_count" : 0,
+      "+1" : 0,
+      "-1" : 0,
+      "laugh" : 0,
+      "hooray" : 0,
+      "confused" : 0,
+      "heart" : 0,
+      "rocket" : 0,
+      "eyes" : 0
+    },
+    "timeline_url" : "",
+    "performed_via_github_app" : null
+  }
+
+
+
+
+
+
