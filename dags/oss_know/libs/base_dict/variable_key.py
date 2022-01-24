4d35de024481eba0670364c522ccbfcb3172bcc5
chenqi <582533558@qq.com>
2022-01-24 13:01:49 +0800

    update codes according to pr comments

--- dags/oss_know/libs/base_dict/variable_key.py
+++ dags/oss_know/libs/base_dict/variable_key.py
@@ -10,8 +10,6 @@ NEED_INIT_GITHUB_PROFILES_REPOS = "need_init_github_profiles_repos"
 NEED_SYNC_GITHUB_ISSUES_REPOS = "need_sync_github_issues_repos"
 NEED_SYNC_GITHUB_PULL_REPUESTS_REPOS = "need_sync_github_pull_repuests_repos"
 NEED_SYNC_GITHUB_COMMITS_REPOS = "need_init_github_commits_repos"
-
-
 NEED_INIT_GITS = "need_init_gits"
 
 MAIL_LISTS = "mail_lists"
--- dags/oss_know/oss_know_dags/dags_maillist/sync_maillist.py
+++ dags/oss_know/oss_know_dags/dags_maillist/sync_maillist.py
@@ -5,8 +5,7 @@ from airflow.models import Variable
 from oss_know.libs.base_dict.variable_key import OPENSEARCH_CONN_DATA, GITHUB_TOKENS, NEED_INIT_GITHUB_COMMITS_REPOS, MAIL_LISTS
 
 # irflow.providers.postgres.hooks.postgres
-# v0.0.1 初始化实现
-# v0.0.2 增加set_github_init_commits_check_data 用于设置初始化后更新的point data
+# v0.0.1 It is a mailing list DAG, git
 
 with DAG(
         dag_id='sync_maillist',
@@ -18,32 +17,22 @@ with DAG(
     def scheduler_init_sync_maillist(ds, **kwargs):
         return 'End::scheduler_init_sync_maillist'
 
-
     op_scheduler_init_sync_maillist = PythonOperator(
         task_id='scheduler_init_sync_maillist',
         python_callable=scheduler_init_sync_maillist
     )
 
-
     def do_sync_maillist(params):
         from oss_know.libs.maillist.archive import sync_archive
         opensearch_conn_info = Variable.get(OPENSEARCH_CONN_DATA, deserialize_json=True)
-
-
-
         sync_archive(opensearch_conn_info, **params)
         return 'End::sync_maillist'
 
-
-
     need_do_inti_sync_ops = []
-
-
     mail_lists = Variable.get(MAIL_LISTS, deserialize_json=True)
 
     for mail_list in mail_lists:
         task_name = mail_list["project_name"]
-        i = 0
         for mail in mail_list["mail_lists"]:
             op_do_sync_maillist = PythonOperator(
                 task_id=f'sync_maillist_{mail["list_name"]}',
