f7ef1b57a0f5a74286bd230ba73e35d566477195
chenqi <582533558@qq.com>
2022-01-25 11:39:08 +0800

    use grimorilab-elk to get mbox

--- dags/oss_know/libs/maillist/archive.py
+++ dags/oss_know/libs/maillist/archive.py
@@ -5,11 +5,21 @@ import gzip
 import json
 from os import makedirs, remove, path
 from opensearchpy import OpenSearch
+from datetime import datetime
+from grimoire_elk.raw.mbox import MBoxOcean
+from grimoire_elk.enriched.mbox import MBoxEnrich
+from grimoire_elk.raw.pipermail import PipermailOcean
+from grimoire_elk.enriched.pipermail import PipermailEnrich
+from grimoire_elk.utils import get_elastic
+from perceval.backends.core.mbox import MBox
+import os
 
 from dateutil.relativedelta import relativedelta
 from perceval.backends.core.mbox import MBox
 from perceval.backends.core.pipermail import Pipermail
 from oss_know.libs.util.opensearch_api import OpensearchAPI
+from oss_know.libs.base_dict.opensearch_index import NSEARCH_INDEX_MAILLISTS
+
 
 
 class EmailArchive:
@@ -77,7 +87,6 @@ class FileArchive(EmailArchive):
 
             # TODO FileArchive should take an 'interval' param to customize the archive interval
             index += relativedelta(months=1)
-
         return urls
 
 
@@ -114,33 +123,59 @@ def sync_archive(opensearch_conn_info, **maillist_params):
         archive = FileArchive(**kwargs)
         archive.get_res()
         repo = MBox(uri=archive.url_prefix, dirpath=archive.dirpath)
+        ocean_backend = MBoxOcean(None)
+        enrich_backend = MBoxEnrich()
     elif archive_type == 'pipermail':
-
         archive = EmailArchive(**kwargs)
         archive.get_res()
         repo = Pipermail(url=archive.url_prefix, dirpath=archive.dirpath)
+        ocean_backend = PipermailOcean(None)
+        enrich_backend = PipermailEnrich()
 
+    # Store original data to raw opensearch index
+    data2es(repo.fetch(), ocean_backend)
+    OS_USER = opensearch_conn_info["USER"]
+    OS_PASS = opensearch_conn_info["PASSWD"]
+    OS_HOST = opensearch_conn_info["HOST"]
+    OS_PORT = opensearch_conn_info["PORT"]
+    OS_URL = f'https://{OS_USER}:{OS_PASS}@{OS_HOST}:{OS_PORT}'
+    elastic_ocean = get_elastic(OS_URL, OPENSEARCH_INDEX_MAILLISTS, True, ocean_backend, [])
+    ocean_backend.set_elastic(elastic_ocean)
 
-    opensearch_api = OpensearchAPI()
-    opensearch_client = OpenSearch(
-        hosts=[{'host': opensearch_conn_info["HOST"], 'port': opensearch_conn_info["PORT"]}],
-        http_compress=True,
-        http_auth=(opensearch_conn_info["USER"], opensearch_conn_info["PASSWD"]),
-        use_ssl=True,
-        verify_certs=False,
-        ssl_assert_hostname=False,
-        ssl_show_warn=False
-    )
-    messages = []
-    num = 0
-    for message in repo.fetch():
-        messages.append(message)
-        num += 1
-        if num >= 1000:
-            opensearch_api.bulk_maillist(opensearch_client, archive.project_name, archive.list_name, messages)
-            num = 0
-            messages = []
+    # TODO What does the param [sortinghat] and [projects] do here?
+    num_enriched = enrich_backend.enrich_items(ocean_backend)
+    print(f'num_enriched: {num_enriched}')
 
-    if messages:
-        opensearch_api.bulk_maillist(opensearch_client, archive.project_name, archive.list_name, messages)
+def data2es(items, ocean):
+    def ocean_item(item):
+        # Hack until we decide when to drop this field
+        if 'updated_on' in item:
+            updated = datetime.fromtimestamp(item['updated_on'])
+            item['metadata__updated_on'] = updated.isoformat()
+        if 'timestamp' in item:
+            ts = datetime.fromtimestamp(item['timestamp'])
+            item['metadata__timestamp'] = ts.isoformat()
+
+        # the _fix_item does not apply to the test data for Twitter
+        try:
+            ocean._fix_item(item)
+        except KeyError:
+            pass
+
+        if ocean.anonymize:
+            ocean.identities.anonymize_item(item)
+
+        return item
+
+    items_pack = []  # to feed item in packs
+
+    for item in items:
+        item = ocean_item(item)
+        if len(items_pack) >= ocean.elastic.max_items_bulk:
+            ocean._items_to_es(items_pack)
+            items_pack = []
+        items_pack.append(item)
+    inserted = ocean._items_to_es(items_pack)
+
+    return inserted
 
