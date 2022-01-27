77778481dc7d425825a4ae5538d85b7cffef3b2d
chenqi <582533558@qq.com>
2022-01-27 13:25:47 +0800

    add infer country api

--- dags/oss_know/libs/util/base.py
+++ dags/oss_know/libs/util/base.py
@@ -2,6 +2,9 @@ import urllib3
 import redis
 from tenacity import *
 from opensearchpy import OpenSearch
+from geopy.geocoders import GoogleV3
+from oss_know.libs.base_dict.infer_file import CCTLD, COMPANY_COUNTRY
+from oss_know.libs.base_dict.variable_key import LOCATIONGEO_TOKEN
 
 from ..util.log import logger
 
@@ -52,3 +55,47 @@ def get_redis_client(redis_client_info):
     redis_client = redis.Redis(host=redis_client_info["HOST"], port=redis_client_info["PORT"], db=0,
                                decode_responses=True)
     return redis_client
+
+def infer_country_from_emailcctld(email):
+    """
+        :param  company: the company given by github
+        :return country_name  : the english name of a country
+        """
+    profile_domain = email.split(".")[-1].upper()
+    if profile_domain in CCTLD.keys():
+        return CCTLD[profile_domain]
+    else:
+        return None
+
+def infer_country_from_emaildomain(email):
+    """
+        :param  company: the company given by github
+        :return country_name  : the english name of a country
+        """
+    emaildomain = company.split("@")[1].split(".")[0].lower().strip()
+    if emaildomain in COMPANY_COUNTRY.keys():
+        return COMPANY_COUNTRY[emaildomain]
+    else:
+        return None
+
+def infer_country_from_location(githubLocation):
+    from airflow.models import Variable
+    """
+        :param  githubLocation: the location given by github
+        :return country_name  : the english name of a country
+        """
+    from airflow.models import Variable
+    api_token = Variable.get(LOCATIONGEO_TOKEN, deserialize_json=True)
+    geolocator = GoogleV3(api_key=api_token)
+    return geolocator.geocode(githubLocation, language='en').address.split(',')[-1].strip()
+
+def infer_country_from_company(company):
+    """
+        :param  company: the company given by github
+        :return country_name  : the english name of a country
+        """
+    company = company.replace("@"," ").lower().strip()
+    if company in COMPANY_COUNTRY.keys():
+        return COMPANY_COUNTRY[company]
+    else:
+        return None
