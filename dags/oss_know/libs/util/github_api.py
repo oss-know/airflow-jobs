d8a3077f18377e3959d7a0db573a1b9cbecf9195
Zhen Ju <juzhen@huawei.com>
2022-03-01 17:40:43 +0800

    Update github_api helper with token proxy accommodator and retry.

--- dags/oss_know/libs/util/github_api.py
+++ dags/oss_know/libs/util/github_api.py
@@ -1,6 +1,6 @@
 import copy
 
-from oss_know.libs.util.base import do_get_result
+from oss_know.libs.util.base import do_get_result, do_get_github_result
 from oss_know.libs.util.log import logger
 
 
@@ -15,25 +15,18 @@ class GithubAPI:
     github_headers = {'Connection': 'keep-alive', 'Accept-Encoding': 'gzip, deflate, br', 'Accept': '*/*',
                       'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36', }
 
-    def get_github_commits(self, http_session, github_tokens_iter, owner, repo, page, since, until):
-        if not http_session or not github_tokens_iter or not owner or not repo or not page or not since or not until:
-            raise GithubAPIException(f"缺少必须的参数")
-
-        url = "https://api.github.com/repos/{owner}/{repo}/commits".format(
-            owner=owner, repo=repo)
+    def get_github_commits(self, http_session, token_proxy_accommodator, owner, repo, page, since, until):
+        url = f"https://api.github.com/repos/{owner}/{repo}/commits"
         headers = copy.deepcopy(self.github_headers)
-        headers.update({'Authorization': 'token %s' % next(github_tokens_iter)})
         params = {'per_page': 100, 'page': page, 'since': since, 'until': until}
 
-        return do_get_result(http_session, url, headers, params)
+        return do_get_github_result(http_session, url, headers, params, token_proxy_accommodator)
 
-    def get_github_issues(self, http_session, github_tokens_iter, owner, repo, page, since):
-        url = "https://api.github.com/repos/{owner}/{repo}/issues".format(
-            owner=owner, repo=repo)
+    def get_github_issues(self, http_session, token_proxy_accommodator, owner, repo, page, since):
+        url = f"https://api.github.com/repos/{owner}/{repo}/issues"
         headers = copy.deepcopy(self.github_headers)
-        headers.update({'Authorization': 'token %s' % next(github_tokens_iter)})
         params = {'state': 'all', 'per_page': 100, 'page': page, 'since': since}
-        res = do_get_result(http_session, url, headers, params)
+        res = do_get_github_result(http_session, url, headers, params, accommodator=token_proxy_accommodator)
 
         logger.info(f"url:{url}, \n headers:{headers}, \n params：{params}")
 
@@ -62,35 +55,30 @@ class GithubAPI:
                     'created_at': '1970-01-01T00:00:00Z', 'updated_at': '1970-01-01T00:00:00Z',
                     'country_inferred_from_email_cctld': '', 'country_inferred_from_email_domain_company': '',
                     'country_inferred_from_location': '', 'country_inferred_from_company': '',
-                    'final_company_inferred_from_company':'',
+                    'final_company_inferred_from_company': '',
                     'company_inferred_from_email_domain_company': '', 'inferred_from_location': ''}
         else:
             logger.info(f"Get GitHub {user_id}'s latest profile from GitHUb.")
             return latest_github_profile
 
-    def get_github_issues_timeline(self, http_session, github_tokens_iter, owner, repo, number, page):
-        url = "https://api.github.com/repos/{owner}/{repo}/issues/{number}/timeline".format(
-            owner=owner, repo=repo, number=number)
+    def get_github_issues_timeline(self, http_session, token_proxy_accommodator, owner, repo, number, page):
+        url = f"https://api.github.com/repos/{owner}/{repo}/issues/{number}/timeline"
         headers = copy.deepcopy(self.github_headers)
-        headers.update({'Authorization': 'token %s' % next(github_tokens_iter)})
         params = {'per_page': 100, 'page': page}
-        res = do_get_result(http_session, url, headers, params)
+        res = do_get_github_result(http_session, url, headers, params, token_proxy_accommodator)
         return res
 
-    def get_github_issues_comments(self, http_session, github_tokens_iter, owner, repo, number, page):
-        url = "https://api.github.com/repos/{owner}/{repo}/issues/{number}/comments".format(
-            owner=owner, repo=repo, number=number)
+    def get_github_issues_comments(self, http_session, token_proxy_accommodator, owner, repo, number, page):
+        url = f"https://api.github.com/repos/{owner}/{repo}/issues/{number}/comments"
         headers = copy.deepcopy(self.github_headers)
-        headers.update({'Authorization': 'token %s' % next(github_tokens_iter)})
         params = {'per_page': 100, 'page': page}
-        res = do_get_result(http_session, url, headers, params)
+        res = do_get_github_result(http_session, url, headers, params, token_proxy_accommodator)
         return res
 
-    def get_github_pull_requests(self, http_session, github_tokens_iter, owner, repo, page, since):
+    def get_github_pull_requests(self, http_session, token_proxy_accommodator, owner, repo, page, since):
         url = "https://api.github.com/repos/{owner}/{repo}/pulls".format(
             owner=owner, repo=repo)
         headers = copy.deepcopy(self.github_headers)
-        headers.update({'Authorization': 'token %s' % next(github_tokens_iter)})
         params = {'state': 'all', 'per_page': 100, 'page': page, 'since': since}
-        res = do_get_result(http_session, url, headers, params)
+        res = do_get_github_result(http_session, url, headers, params, token_proxy_accommodator)
         return res
