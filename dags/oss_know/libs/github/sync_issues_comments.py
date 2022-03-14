6fa0cdad612d2041188440c488f09d984d6698b0
fivestarsky <fivestarsky@gmail.com>
2022-03-14 19:54:41 +0800

    move github request sleep time to options.py

--- dags/oss_know/libs/github/sync_issues_comments.py
+++ dags/oss_know/libs/github/sync_issues_comments.py
@@ -71,7 +71,7 @@ def sync_github_issues_comments(github_tokens, opensearch_conn_info, owner, repo
 
         for page in range(1, 10000):
             # Token sleep
-            time.sleep(random.uniform(0.05, 0.1))
+            time.sleep(random.uniform(GITHUB_SLEEP_TIME_MIN, GITHUB_SLEEP_TIME_MAX))
             
             req = github_api.get_github_issues_comments(session, github_tokens_iter, owner, repo, issues_number, page)
             one_page_github_issues_comments = req.json()
---
+++ data_schema/grafana/Echarts/timezone.js
@@ -0,0 +1,57 @@
+// function demoOption 不要复制 ！！！
+function demoOption(data, theme, echartsInstance, echarts) {
+
+    var data_array = new Array();
+
+    for (let i = 0; i < data.series[0].fields[0].values.length; i++) {
+        data_array.push({
+            name: data.series[0].fields[0].values.buffer[i],
+            value: data.series[0].fields[1].values.buffer[i]
+        });
+    }
+
+    var option = {
+        backgroundColor: 'transparent',
+        title: {
+            text: '代码提交时区',
+            left: 'center',
+            top: 'center'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>时区:{b};<br/>提交量:{c}<br/>百分比:({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            top: '20%'
+        },
+        series: [
+            {
+                name: '时区详细信息',
+                type: 'pie',
+                label: {
+                    normal: {
+                        formatter: '{b}:{c}: ({d}%)',
+                        textStyle: {
+                            fontWeight: 'normal',
+                            fontSize: 15
+                        }
+                    }
+                },
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                },
+                radius: ['40%', '70%'],
+                data: data_array
+            }
+        ]
+    };
+
+    return option;
+
+}
