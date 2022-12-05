06ec848e447746bc25984f4f392a0b8c664ab0c5
Zhen Ju <juzhenatpku@gmail.com>
2022-12-05 13:06:16 +0800

    Add daily github_commits sync from other clickhouse env

--- dags/oss_know/oss_know_dags/dags_daily_sync_clickhouse/dag_daily_github_commits_sync_from_clickhouse.py
+++ dags/oss_know/oss_know_dags/dags_daily_sync_clickhouse/dag_daily_github_commits_sync_from_clickhouse.py
@@ -11,24 +11,24 @@ from oss_know.libs.clickhouse.sync_clickhouse_data import sync_from_remote_by_re
 clickhouse_conn_info = Variable.get(CLICKHOUSE_DRIVER_INFO, deserialize_json=True)
 sync_from_clickhouse_conn_info = Variable.get(SYNC_FROM_CLICKHOUSE_DRIVER_INFO, deserialize_json=True)
 
-# Daily sync gits data from other clickhouse environment by owner/repo
-with DAG(dag_id='daily_gits_sync_from_clickhouse',  # schedule_interval='*/5 * * * *',
+# Daily sync github_commits data from other clickhouse environment by owner/repo
+with DAG(dag_id='daily_github_commits_sync_from_clickhouse',  # schedule_interval='*/5 * * * *',
          schedule_interval=None, start_date=datetime(2021, 1, 1), catchup=False,
          tags=['github', 'daily sync clickhouse'], ) as dag:
     def do_init():
-        return 'Start init_daily_gits_sync'
+        return 'Start init_daily_github_commits_sync'
 
 
-    op_init = PythonOperator(task_id='op_init_daily_gits_sync_from_clickhouse', python_callable=do_init)
+    op_init = PythonOperator(task_id='op_init_daily_github_commits_sync_from_clickhouse', python_callable=do_init)
 
 
-    def do_sync_gits_from_clickhouse_by_group(params):
+    def do_sync_github_commits_from_clickhouse_by_group(params):
         sync_from_remote_by_repos(clickhouse_conn_info, sync_from_clickhouse_conn_info,
-                                  "gits",
+                                  "github_commits",
                                   params.get('owner_repos'))
 
 
-    all_owner_repos = union_remote_owner_repos(clickhouse_conn_info, sync_from_clickhouse_conn_info, "gits")
+    all_owner_repos = union_remote_owner_repos(clickhouse_conn_info, sync_from_clickhouse_conn_info, "github_commits")
 
     # Init 26 sub groups by letter(to make the task DAG static)
     # Split all tasks into 26 groups by their capital letter, all tasks inside a group are executed sequentially
@@ -42,13 +42,13 @@ with DAG(dag_id='daily_gits_sync_from_clickhouse',  # schedule_interval='*/5 * *
         task_groups_by_capital_letter[owner[0].lower()].append(owner_repo_pair)
 
     for letter, owner_repos in task_groups_by_capital_letter.items():
-        op_sync_gits_from_clickhouse_group = PythonOperator(
-            task_id=f'op_sync_gits_from_clickhouse_group_{letter}',
-            python_callable=do_sync_gits_from_clickhouse_by_group,
+        op_sync_github_commits_from_clickhouse_group = PythonOperator(
+            task_id=f'op_sync_github_commits_from_clickhouse_group_{letter}',
+            python_callable=do_sync_github_commits_from_clickhouse_by_group,
             op_kwargs={
                 "params": {
                     "owner_repos": owner_repos
                 }
             }
         )
-        op_init >> op_sync_gits_from_clickhouse_group
+        op_init >> op_sync_github_commits_from_clickhouse_group
