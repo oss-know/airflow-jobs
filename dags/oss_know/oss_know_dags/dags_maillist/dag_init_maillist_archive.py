295cc026c21f42c02f86b11a341ac8b1d75c79c9
Zhen Ju <juzhenatpku@gmail.com>
2023-01-04 12:25:54 +0800

    Rename the sync mail list archive to 'init mail list'

--- dags/oss_know/oss_know_dags/dags_maillist/dag_init_maillist_archive.py
+++ dags/oss_know/oss_know_dags/dags_maillist/dag_init_maillist_archive.py
@@ -9,38 +9,37 @@ from oss_know.libs.base_dict.variable_key import OPENSEARCH_CONN_DATA, MAIL_LIST
 # v0.0.1 It is a mailing list DAG
 
 with DAG(
-        dag_id='init_maillist_archive',
+        dag_id='init_mail_list_archive',
         schedule_interval=None,
         start_date=datetime(2000, 1, 1),
         catchup=False,
         tags=['email'],
 ) as dag:
-    def scheduler_init_sync_maillist(ds, **kwargs):
-        return 'End::scheduler_init_sync_maillist'
+    def scheduler_init_mail_list(ds, **kwargs):
+        return 'End::scheduler_init_mail_list'
 
 
-    op_scheduler_init_sync_maillist = PythonOperator(
-        task_id='scheduler_init_sync_maillist',
-        python_callable=scheduler_init_sync_maillist
+    op_scheduler_init_mail_list = PythonOperator(
+        task_id='scheduler_init_mail_list',
+        python_callable=scheduler_init_mail_list
     )
 
 
-    def do_sync_maillist(params):
-        from oss_know.libs.maillist.archive import sync_archive
+    def do_init_mail_list(params):
+        from oss_know.libs.maillist.archive import init_archive
         opensearch_conn_info = Variable.get(OPENSEARCH_CONN_DATA, deserialize_json=True)
-        sync_archive(opensearch_conn_info, **params)
-        return 'End::sync_maillist'
+        init_archive(opensearch_conn_info, **params)
+        return 'End::init_mail_list'
 
 
-    need_do_inti_sync_ops = []
     mail_lists = Variable.get(MAIL_LISTS, deserialize_json=True)
 
     for mail_list in mail_lists:
         task_name = mail_list["project_name"]
         for mail in mail_list["mail_lists"]:
-            op_do_sync_maillist = PythonOperator(
-                task_id=f'sync_maillist_{mail["list_name"]}',
-                python_callable=do_sync_maillist,
+            op_do_init_mail_list = PythonOperator(
+                task_id=f'init_mail_list_{mail["list_name"]}',
+                python_callable=do_init_mail_list,
                 op_kwargs={'params': dict({"project_name": task_name}.items() | mail.items())},
             )
-            op_scheduler_init_sync_maillist >> op_do_sync_maillist
+            op_scheduler_init_mail_list >> op_do_init_mail_list
