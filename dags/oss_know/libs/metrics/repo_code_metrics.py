f66e0f1b3c1548982ad099ad2055fb531c920a24
Zhen Ju <juzhenatpku@gmail.com>
2023-09-05 15:59:14 +0800

    Optimize and correct repo code metrics calculation utils.
    - Optimize imports and logs and code styles
    - Handle the situation when the repo is unavailable
    - Do not remove the repo after calculation

--- dags/oss_know/libs/metrics/repo_code_metrics.py
+++ dags/oss_know/libs/metrics/repo_code_metrics.py
@@ -1,44 +1,37 @@
 import datetime
-import json
-from collections import defaultdict
-from statistics import mean
-from oss_know.libs.util.log import logger
-
-import lizard
-from lizard import OutputScheme
-
-from oss_know.libs.metrics.influence_metrics import MetricRoutineCalculation
-from oss_know.libs.base_dict.variable_key import REPO_CLONE_DIR
 import os
 import shutil
-import git # GitPython
+
+import git  # GitPython
+import lizard
+
+from oss_know.libs.base_dict.variable_key import REPO_CLONE_DIR
+from oss_know.libs.metrics.influence_metrics import MetricRoutineCalculation
+from oss_know.libs.util.log import logger
+
 
 def clone_(owner, repo, dir):
     url = f"https://github.com/{owner}/{repo}.git"
 
     try:
-        if os.path.exists(os.path.join(dir, '.git')):
+        if os.path.exists(dir):
             repo = git.Repo(dir)
             o = repo.remotes.origin
             o.pull()
         else:
             git.Repo.clone_from(url, dir)
     except git.exc.GitError as e:
-        print(f'ERROR! {url} : {str(e)}')
-        return 
+        logger.error(f'Failed to fetch code {url}, error: {str(e)}')
+        return
 
 
 def remove_repos(dir):
     if os.path.exists(dir):
         shutil.rmtree(dir)
-        os.rmdir(dir)
 
 
 def extract_metrics(owner, repo_name, repo_dir):
-    metric = {}
-    metric['owner'] = owner
-    metric['repo'] = repo_name
-    metric['created_at'] = datetime.datetime.now()
+    metric = {'owner': owner, 'repo': repo_name, 'created_at': datetime.datetime.now()}
 
     repo = git.Repo(repo_dir)
     sha = repo.head.commit.hexsha
@@ -53,31 +46,41 @@ def extract_metrics(owner, repo_name, repo_dir):
     metric['average_cyclomatic_complexity'] = res.as_fileinfo().average_cyclomatic_complexity
     metric['average_token_count'] = res.as_fileinfo().average_token_count
 
-    return metric 
+    return metric
 
 
 class RepoCodeMetricRoutineCalculation(MetricRoutineCalculation):
+
     def calculate_metrics(self):
-        logger.info(f'calculating by {self.owner}, {self.repo}')
+        logger.info(f'Calculating repo code metric for {self.owner}/{self.repo}')
         dir = f"{REPO_CLONE_DIR}/{self.owner}/{self.repo}"
-        clone_(self.owner, self.repo, dir, True)
-        calculated_metrics = extract_metrics(self.owner, self.repo, dir)
-        remove_repos(dir)
+        clone_(self.owner, self.repo, dir)
+
+        calculated_metrics = extract_metrics(self.owner, self.repo, dir) if os.path.exists(dir) else None
+        # remove_repos(dir)
         return calculated_metrics
 
     def save_metrics(self):
-        logger.info(f'save metrics with {len(self.batch)} records, to {self.table_name}')
+        logger.info(f'Save code repo metrics with {len(self.batch)} records, to {self.table_name}')
         cursor = self.mysql_conn.cursor()
-        insert_query = "INSERT INTO repo_code_metrics (owner, repo, created_at, sha, nloc, average_nloc, average_cyclomatic_complexity, average_token_count) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"
+        insert_query = """
+        INSERT INTO repo_code_metrics (owner, repo, created_at, sha,
+        nloc, average_nloc, average_cyclomatic_complexity, average_token_count) 
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"""
         cursor.executemany(insert_query, self.batch)
         self.mysql_conn.commit()
-        logger.info("Data inserted successfully!")
+        logger.info(f"{len(self.batch)} records on {self.owner}/{self.repo} inserted successfully!")
         cursor.close()
 
-    metrics_keys = ["owner", "repo", "created_at", "sha", "nloc", "average_nloc", "average_cyclomatic_complexity", "average_token_count"]
+    metrics_keys = ["owner", "repo", "created_at", "sha", "nloc", "average_nloc",
+                    "average_cyclomatic_complexity", "average_token_count"]
 
     def routine_calculate_metrics_once(self):
         metrics = self.calculate_metrics()
+        if not metrics:
+            logger.warning(f'No metrics calculated for {self.owner}/{self.repo}, skip')
+            return
+
         metrics_values = []
         for key in RepoCodeMetricRoutineCalculation.metrics_keys:
             metrics_values.append(metrics[key])
