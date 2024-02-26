4864d1dec299e6be9abe88b3e28c262a64edea42
Zhen Ju <juzhenatpku@gmail.com>
2024-02-26 18:51:13 +0800

    Add utils to sync github release from opensearch to clickhouse

--- dags/oss_know/libs/base_dict/clickhouse.py
+++ dags/oss_know/libs/base_dict/clickhouse.py
@@ -25,3 +25,40 @@ GITHUB_ISSUES_TIMELINE_TEMPLATE = {
         "timeline_raw": ""
     }
 }
+
+GITHUB_RELEASE_TEMPLATE = {
+    "search_key": {
+        "owner": "",
+        "repo": "",
+        "updated_at": 1708934629565
+    },
+    "raw_data": {
+        "html_url": "https://github.com/OWNER/REPO/releases/tag/v1.29.2",
+        "id": 142028094,
+        "author": {
+            "login": "robot",
+            "id": 33505452,
+            "type": "User"
+        },
+        "tag_name": "TAG_NAME",
+        "target_commitish": "master",
+        "name": "RELEASE_NAME",
+        "draft": False,
+        "prerelease": False,
+        "created_at": "2024-02-14T10:32:39Z",
+        "published_at": "2024-02-14T18:01:57Z",
+        "body": "release desc",
+        "reactions": {
+            "url": "https://api.github.com/repos/OWNER/REPO/releases/142028094/reactions",
+            "total_count": 31,
+            "+1": 14,
+            "-1": 0,
+            "laugh": 2,
+            "hooray": 7,
+            "confused": 0,
+            "heart": 3,
+            "rocket": 3,
+            "eyes": 2
+        }
+    }
+}
